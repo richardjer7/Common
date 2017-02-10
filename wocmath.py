fe6489abf95864b2a01a7abf2c819e12b881dc19
Yuchen Huang <oocasthy@umich.edu>
2017-02-10 12:30:06 -0500

    Fix equation

--- wocmath.py
+++ wocmath.py
@@ -1,7 +1,7 @@
 import cozmo
 
 def running_average_pos(old: cozmo.util.Position, new: cozmo.util.Position, a: float):
-    xx = new.x * a + old.x * a
-    yy = new.y * a + old.y * a
-    zz = new.z * a + old.z * a
+    xx = new.x * a + old.x * (1-a)
+    yy = new.y * a + old.y * (1-a)
+    zz = new.z * a + old.z * (1-a)
     return cozmo.util.Position(xx,yy,zz)
