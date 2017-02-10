f94f8d3e3f1963c12f9b3bb0ce0398b65d339a9e
Yuchen Huang <oocasthy@umich.edu>
2017-02-10 12:39:29 -0500

    Add squared distance function

--- wocmath.py
+++ wocmath.py
@@ -5,3 +5,9 @@ def running_average_pos(old: cozmo.util.Position, new: cozmo.util.Position, a: f
     yy = new.y * a + old.y * (1-a)
     zz = new.z * a + old.z * (1-a)
     return cozmo.util.Position(xx,yy,zz)
+
+def distance2(pos1: cozmo.util.Position, pos2: cozmo.util.Position):
+    xx = pos1.x - pos2.x
+    yy = pos1.y - pos2.y
+    zz = pos1.z - pos2.z
+    return xx * xx + yy * yy + zz * zz
