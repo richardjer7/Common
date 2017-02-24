59b887486eb00bbdab547a54a8c594652ec2562c
Yuchen Huang <oocasthy@umich.edu>
2017-02-24 12:14:17 -0500

    Add linear and log mapping functions

--- wocmath.py
+++ wocmath.py
@@ -1,4 +1,5 @@
 import cozmo
+from math import log
 
 def running_average_pos(old: cozmo.util.Position, new: cozmo.util.Position, a: float):
     xx = new.x * a + old.x * (1-a)
@@ -11,3 +12,20 @@ def distance2(pos1: cozmo.util.Position, pos2: cozmo.util.Position):
     yy = pos1.y - pos2.y
     zz = pos1.z - pos2.z
     return xx * xx + yy * yy + zz * zz
+
+def linear_mapping(x1:float, y1:float, x2:float, y2:float, vx:float):
+    # y = k*x + b
+    k = (y2 - y1) / (x2 - x1)
+    b = y1 - k * x1
+    return k * vx + b
+
+def log_mapping(x1:float, y1:float, x2:float, y2:float, vx:float, base:float):
+    xl1 = log(x1, base)
+    xl2 = log(x2, base)
+    vxl = log(vx, base)
+    return linear_mapping(xl1, y1, xl2, y2, vxl)
+    
+# test case
+if __name__ == '__main__':
+    r = log_mapping(10,1,100,2,50,10)
+    print(r)
