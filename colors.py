4c55d0b0ec1bf6b1f84a3cc50d67abc0f29373f4
Sharan Shodhan <sharanshodhan@gmail.com>
2017-04-13 12:38:29 -0400

    Added blinking colours

--- colors.py
+++ colors.py
@@ -10,11 +10,42 @@ Helper class for Cozmo light colors
 
 class Colors:
     """ Class to define common colors to be used """
-    CYAN = Light(Color(name="cyan", int_color=0x00ffffff))
+    CYAN_2 = Light(on_color=Color(name="cyan", int_color=0x00ffffff), off_color=Color(name="black", int_color=0x00000000), on_period_ms=1000, off_period_ms=500);
+    GRAY_2 = Light(on_color=Color(name="gray", int_color=0x808080ff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    MAGENTA_2 = Light(on_color=Color(name="magenta", int_color=0xff00ffff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    YELLOW_2 = Light(on_color=Color(name="yellow", int_color=0xe5e500ff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    GREEN_2 = Light(on_color=Color(name="green", int_color=0x00ff00ff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    RED_2 = Light(on_color=Color(name="red", int_color=0xff0000ff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    BLUE_2 = Light(on_color=Color(name="blue", int_color=0x0000ffff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    WHITE_2 = Light(on_color=Color(name="white", int_color=0xffffffff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+    TEAL_2 = Light(on_color=Color(name="teal", int_color=0x009688ff), off_color=Color(name="black", int_color=0x00000000),on_period_ms=1000, off_period_ms=500);
+
+    CYAN_1 = Light(on_color=Color(name="cyan", int_color=0x00ffffff),
+                   off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    GRAY_1 = Light(on_color=Color(name="gray", int_color=0x808080ff),
+                   off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    MAGENTA_1 = Light(on_color=Color(name="magenta", int_color=0xff00ffff),
+                      off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    YELLOW_1 = Light(on_color=Color(name="yellow", int_color=0xe5e500ff),
+                     off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    GREEN_1 = Light(on_color=Color(name="green", int_color=0x00ff00ff),
+                    off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    RED_1 = Light(on_color=Color(name="red", int_color=0xff0000ff), off_color=Color(name="black", int_color=0x00000000),
+                  on_period_ms=200, off_period_ms=200);
+    BLUE_1 = Light(on_color=Color(name="blue", int_color=0x0000ffff),
+                   off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    WHITE_1 = Light(on_color=Color(name="white", int_color=0xffffffff),
+                    off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+    TEAL_1 = Light(on_color=Color(name="teal", int_color=0x009688ff),
+                   off_color=Color(name="black", int_color=0x00000000), on_period_ms=200, off_period_ms=200);
+
+    GRAY = Light(Color(name="gray", int_color=0x808080ff))
     MAGENTA = Light(Color(name="magenta", int_color=0xff00ffff))
-    YELLOW = Light(Color(name="yellow", int_color=0xffff00ff))
+    YELLOW = Light(Color(name="yellow", int_color=0xe5e500ff))
     GREEN = Light(Color(name="green", int_color=0x00ff00ff))
     RED = Light(Color(name="red", int_color=0xff0000ff))
     BLUE = Light(Color(name="blue", int_color=0x0000ffff))
     WHITE = Light(Color(name="white", int_color=0xffffffff))
+    TEAL = Light(Color(name="teal", int_color=0x009688ff))
+
     OFF = Light(Color(name="off"))
--- woc.py
+++ woc.py
@@ -8,12 +8,20 @@ import pygame
 class WOC:
     def __init__(self):
         pygame.init()
+        self._exit_flag = False
 
     @property
     def exit_flag(self):
+        if (self._exit_flag == True):
+            return True
         for event in pygame.event.get():
             if (event.type == pygame.KEYUP):
                 if (event.key == pygame.K_ESCAPE):
                     print("EXIT")
                     return True
         return False
+
+    @exit_flag.setter
+    def exit_flag(self, value):
+        print("Setting exit flag");
+        self._exit_flag = True;
